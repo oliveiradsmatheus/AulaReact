b6cdb3d146ccdf7ecf74ca2837076e3bd577b2fe
oliveiradsmatheus <oliveiradsmatheus@gmail.com>
2024-08-19 10:09:08 -0300

    Criando um relógio com atualização automática

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Data from "./meusComponentes/Data";
 function App() {
   return (
     <div className="App">
-        <Data texto = "Data e hora atuais: "/>
+        <Data texto = "Data e hora atuais: " timeZone="-3.00"/>
+        <Data texto = "Data e hora em Londres: " timeZone="+4.00"/>
+        <Data texto = "Data e hora em Nova York: " timeZone="-1.00"/>
     </div>
   );
 }
--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -5,24 +5,35 @@ export default class Data extends Component{
     constructor (props){
         super(); // Executa o construtor do pai
         this.props = props;
-        this.dataAtual = new Date().toLocaleString();
+        this.state = {dataAtual: new Date().toLocaleString()}
+    }
+
+    pegaDataDe(timeZone){
+        const dataAtual = new Date();
+        let timeZoneFromDB = Number.parseInt(timeZone);
+        let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
+        return Date(dataAtual.getTime() + diferencaTempo * 60 * 1000)
     }
 
     // Fase de montagem
     componentDidMount(){
         console.log("O componente foi montado.");
-        this.dataAtual = new Date().toLocaleString();
+        // Não é permitido atualizar o estado do componente de forma direta (this.state = ...)
+        this.setState({dataAtual: new Date().toLocaleString()});
     }
 
     // Fase de atualização
     componentDidUpdate(){
-        console.log("O componente foi atualizado");
+        console.log("O componente foi atualizado.");
+        setTimeout(()=>{
+            this.setState({dataAtual: new Date().toLocaleString()});
+        },1000);
     }
 
     // Sobrescrita de método
     render(){
         return (
-            <h1>{ this.props.texto || ""}{this.dataAtual}</h1>
+            <h1>{ this.props.texto || ""}{this.state.dataAtual}</h1>
         )
     }
 }
