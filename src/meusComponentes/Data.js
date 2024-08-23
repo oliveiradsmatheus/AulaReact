cd1694f32c6292e3529fd7b64e07e2d44dc83678
oliveiradsmatheus <oliveiradsmatheus@gmail.com>
2024-08-23 08:59:44 -0300

    Corrigindo o funcionamento do componente data

--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -5,14 +5,16 @@ export default class Data extends Component{
     constructor (props){
         super(); // Executa o construtor do pai
         this.props = props;
-        this.state = {dataAtual: new Date().toLocaleString()}
+        this.state = {dataAtual: new Date().toLocaleString()};
     }
 
     pegaDataDe(timeZone){
         const dataAtual = new Date();
-        let timeZoneFromDB = Number.parseInt(timeZone);
+        let timeZoneFromDB = parseInt(timeZone);
         let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
-        return Date(dataAtual.getTime() + diferencaTempo * 60 * 1000)
+        let milisegundos = parseInt(dataAtual.getTime() + (diferencaTempo * 60 * 1000));
+        const data = new Date(milisegundos);
+        return data;
     }
 
     // Fase de montagem
@@ -26,7 +28,9 @@ export default class Data extends Component{
     componentDidUpdate(){
         console.log("O componente foi atualizado.");
         setTimeout(()=>{
-            this.setState({dataAtual: new Date().toLocaleString()});
+            this.setState({
+                dataAtual: this.pegaDataDe(this.props.timeZone).toLocaleString()
+            });
         },1000);
     }
 
